66d94173c5c6c2c6702dae8015069e450a6c881d
Mell <julietmell44@gmail.com>
2025-03-25 11:48:55 -0400

    ta hecho el codigo

---
+++ Quote.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+def main(page: ft.Page):
+    def button_clicked(e):
+        if cg.value:
+            t.value = cg.value
+            img.src = images[cg.value]
+            page.update()
+
+    images = {
+        "May your dreams be bigger than your fears": "/Users/coccoo/Desktop/gringo.jpg",
+        "Que tus sueños sean más grandes que tus miedos": "/Users/coccoo/Desktop/nick.jpg",
+        "Che i tuoi sogni siano più grandi delle tue paure": "/Users/coccoo/Desktop/italiano.jpg"
+    }
+
+    t = ft.Text()
+    img = ft.Image(width=300, height=200, fit=ft.ImageFit.COVER)
+    b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+
+    cg = ft.RadioGroup(
+        content=ft.Column([
+            ft.Radio(value="May your dreams be bigger than your fears", label="Inglés"),
+            ft.Radio(value="Que tus sueños sean más grandes que tus miedos", label="Español"),
+            ft.Radio(value="Che i tuoi sogni siano più grandi delle tue paure", label="Italiano")
+        ])
+    )
+
+    page.add(
+        ft.Text("Cambia el idioma de la frase: 'Que tus sueños sean más grandes que tus miedos'"),
+        cg, b, t, img
+    )
+
+ft.app(main)
